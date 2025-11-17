a531b9ac5b844993d6e06a36072ed4fa098dda28
kangcheolung <skhu202114111@gmail.com>
2025-11-17 19:20:55 +0900

    init commit

---
+++ src/test/java/com/cotato/backend/JwtbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cotato.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
